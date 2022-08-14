57a2eef89c40c0b0acb6a82055174e1d04940e41
ajay <ajay.pratap.singh.thakur.1996@gmail.com>
2022-08-14 20:29:58 +0530

    minor changes

--- test/ERC20StanderdToken.ts
+++ test/ERC20StanderdToken.ts
@@ -4,6 +4,7 @@ import { ethers } from 'hardhat'
 import { expandToDecimals } from './utils'
 
 describe('ERC20StanderdToken', function () {
+  
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshopt in every test.
